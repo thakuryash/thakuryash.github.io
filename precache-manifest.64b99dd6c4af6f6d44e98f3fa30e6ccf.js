f1142304bd077c01a2152b3a78bc96df5d8d9ec0
ythakur <thakuryash@live.com>
2023-06-19 14:29:56 -0400

    Updates

---
+++ precache-manifest.64b99dd6c4af6f6d44e98f3fa30e6ccf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e04ea6b66e7bdcedd2d6",
+    "url": "/ythakur/static/js/main.e04ea6b6.chunk.js"
+  },
+  {
+    "revision": "3a8a6ab0a5619d173ab3",
+    "url": "/ythakur/static/js/1.3a8a6ab0.chunk.js"
+  },
+  {
+    "revision": "c69759fc029cc938f613",
+    "url": "/ythakur/static/js/runtime~main.c69759fc.js"
+  },
+  {
+    "revision": "8753ec9143f7464106dd7d55d2bda061",
+    "url": "/ythakur/static/media/yash_thakur.8753ec91.jpeg"
+  },
+  {
+    "revision": "144f5c09a5c314603678d009c3c88b68",
+    "url": "/ythakur/static/media/Technician.144f5c09.png"
+  },
+  {
+    "revision": "618c5dd170c4490a0ddef2db5e653596",
+    "url": "/ythakur/static/media/Caloriestracker.618c5dd1.png"
+  },
+  {
+    "revision": "a07d1ac80eca5f8e89b2df27042675af",
+    "url": "/ythakur/static/media/blog1.a07d1ac8.jpg"
+  },
+  {
+    "revision": "e79e23bc96bbd293e16c8553710d196a",
+    "url": "/ythakur/static/media/blog2.e79e23bc.jpg"
+  },
+  {
+    "revision": "79cca39b7a5a8d2873d60f5a112459dd",
+    "url": "/ythakur/static/media/blog3.79cca39b.jpg"
+  },
+  {
+    "revision": "c9ce2e10192f0b08fb9a49ae7673472b",
+    "url": "/ythakur/static/media/blog4.c9ce2e10.jpg"
+  },
+  {
+    "revision": "af30c1e7315ae2c0ed4fbac4a564d8e4",
+    "url": "/ythakur/index.html"
+  }
+];
