eb655ad07ae0e378eadbd9cfbc24c492708e06c9
Yash <greentronics.yash@gmail.com>
2025-10-08 12:38:13 -0400

    Routing fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import GlobalStyled from './styles/globalstyle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import GlobalStyled from "./styles/globalstyle";
 
-const element = (<div>
-  <GlobalStyled />
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</div>)
+const element = (
+  <div>
+    <GlobalStyled />
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
- element
-);
+root.render(element);
